4d5b03f2d9478a109dc228d5d89b45784672983a
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-04-12 11:39:32 +0200

    Updated constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,13 @@
 export const DEFAULT_LOCALE: string = 'english'
-export const SUPPORTED_LOCALES: string[] = [ 'english', 'french' ]
+export const SUPPORTED_LOCALES: string[] = [
+    'english',
+    'french'
+]
 export const DEFAULT_LANGUAGE: string = 'en'
-export const LANGUAGE_MAPPINGS: {[key:string]: string} = {
+export const LANGUAGE_MAPPINGS = {
     english: 'en',
     french: 'fr'
 }
 export const INTENT_PROBABILITY_THRESHOLD: number =  0.4
-export const SLOT_CONFIDENCE_THRESHOLD: number =  0.5
+export const SLOT_CONFIDENCE_THRESHOLD: number =  0.45
 export const ASR_UTTERANCE_CONFIDENCE_THRESHOLD: number =  0.5
