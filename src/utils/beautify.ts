2584678f9d9f06892b5e57fd0329c4551d184e6f
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-04-19 10:51:09 +0200

    Now using moment to format dates

--- src/utils/beautify.ts
+++ src/utils/beautify.ts
@@ -1,29 +1,32 @@
-import { configFactory } from '../factories'
+import { configFactory, i18nFactory } from '../factories'
+import { LANGUAGE_MAPPINGS } from '../constants'
+import moment from 'moment'
+import 'moment/locale/fr'
 
-export const beautify = {    
-    time: (date: Date, cancelTimezoneOffset: boolean = false): string => {
+export const beautify = {
+    date: (date: Date): string => {
+        const i18n = i18nFactory.get()
         const config = configFactory.get()
-        let options: any = { hour: 'numeric', minute: 'numeric' }
+        const language = LANGUAGE_MAPPINGS[config.locale]
+
+        return moment(date).locale(language).format(i18n('moment.date'))
+    },
+
+    time: (date: Date, cancelTimezoneOffset: boolean = false): string => {
+        const i18n = i18nFactory.get()
+        const config = configFactory.get()
+        const language = LANGUAGE_MAPPINGS[config.locale]
 
         if (cancelTimezoneOffset) {
-            options = {
-                ...options,
-                timeZone: 'UTC'
-            }
+            return moment.utc(date)
+                .locale(language)
+                .format(i18n('moment.time'))
+                .replace(' 0', '')
         }
 
-        if (config.locale === 'french') {
-            // French
-            return date.toLocaleString('fr-FR', {
-                ...options,
-                hour12: false
-            }).replace(':', ' heure ').replace(' 00', '')
-        } else {
-            // English
-            return date.toLocaleString('en-US', {
-                ...options,
-                hour12: true
-            }).replace(':', ' ').replace(' 00','')
-        }
+        return moment(date)
+            .locale(language)
+            .format(i18n('moment.time'))
+            .replace(' 0', '')
     }
 }
