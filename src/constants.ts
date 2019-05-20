a7b2aadd7a1968ef59880096101807d6e7db2d89
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-05-20 17:17:12 +0200

    Updated assets path

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
+export const ASSETS_PATH = `${process.cwd()}/assets`
 export const INTENT_PROBABILITY_THRESHOLD: number =  0.4
 export const SLOT_CONFIDENCE_THRESHOLD: number =  0.45
 export const ASR_UTTERANCE_CONFIDENCE_THRESHOLD: number =  0.5
--- src/utils/mappings.ts
+++ src/utils/mappings.ts
@@ -1,10 +1,13 @@
+import { ASSETS_PATH } from '../constants'
+import fs from 'fs'
+
 let _mappings: any = null
 
-function init (language: string) {
+function init(language: string) {
     _mappings = {
-        city: require(`../../assets/mappings/${language}/city.json`),
-        country: require(`../../assets/mappings/${language}/country.json`),
-        region: require(`../../assets/mappings/${language}/region.json`)
+        city: JSON.parse(fs.readFileSync(`${ASSETS_PATH}/mappings/${language}/city.json`, 'utf8')),
+        country: JSON.parse(fs.readFileSync(`${ASSETS_PATH}/mappings/${language}/country.json`, 'utf8')),
+        region: JSON.parse(fs.readFileSync(`${ASSETS_PATH}/mappings/${language}/region.json`, 'utf8'))
     }
 }
 
